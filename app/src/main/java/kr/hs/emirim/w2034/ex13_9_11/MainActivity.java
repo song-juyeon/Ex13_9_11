470829af1cff1652104f68a7b7bfa22710a3995c
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-07-30 11:18:21 +0900

    Edit MainActivity.java(Add TextView) Commit

--- app/src/main/java/kr/hs/emirim/w2034/ex13_9_11/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2034/ex13_9_11/MainActivity.java
@@ -7,9 +7,11 @@ import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     SeekBar seek1, seek2;
+    TextView tv1, tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,28 +21,32 @@ public class MainActivity extends AppCompatActivity {
 
         seek1 = findViewById(R.id.seek1);
         seek2 = findViewById(R.id.seek2);
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
         Button btnStart = findViewById(R.id.btn_start);
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Thread thread1 = new Thread(){
+                new Thread(){
                     @Override
                     public void run() {
                         for (int i=seek1.getProgress(); i < 100; i=i+2) {
                             seek1.setProgress(seek1.getProgress() + 2);
+                            tv1.setText(R.string.tv1);
+                            tv1.append(" "+seek1.getProgress() + "%");
                             SystemClock.sleep(100);
                         }
                     }
-                };
-
-                thread1.start();
+                }.start();
 
                 Thread thread2 = new Thread(){
                     @Override
                     public void run() {
                         for (int i=seek2.getProgress(); i < 100; i++) {
                             seek2.setProgress(seek2.getProgress() + 1);
+                            tv2.setText(R.string.tv2);
+                            tv2.append(" "+seek2.getProgress() + "%");
                             SystemClock.sleep(100);
                         }
                     }
